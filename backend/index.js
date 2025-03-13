e3e385ddc9db3c581710da71aa73c1cfd9499638
Jawad <jawadal829@gmail.com>
2025-03-13 14:04:50 +0500

    updated connection

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const connectDatabase = require('./config/connectDatabase');
 require('dotenv').config({
     path: './config/.env'
 })
 app.use(express.json());
 app.use(cors());
+connectDatabase()
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`server running`);
 });
